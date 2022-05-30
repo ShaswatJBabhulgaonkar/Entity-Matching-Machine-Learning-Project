88d0a7e218c79a594bbae268cb63f2fce3033d3d
Shaswat Babhulgaonkar <56784741+ShaswatJBabhulgaonkar@users.noreply.github.com>
2022-05-30 14:05:48 +0530

    Update tf_backend.tf

--- infra/dev/glue/tf_backend.tf
+++ infra/dev/glue/tf_backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "glue" {
-    bucket         = "h1-data-science-non-prod-tf-state"
+    bucket         = "data-science-tf-state"
     key            = "data-science/dev/glue/tf.state"
     region         = "us-east-2"
-    dynamodb_table = "h1datascience-dev-tf-infra"
+    dynamodb_table = "datascience-dev-tf-infra"
   }
 }
