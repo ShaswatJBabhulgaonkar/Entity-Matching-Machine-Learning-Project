5881926933bbcef3f039eac3df04276b2e7d186f
Shaswat Babhulgaonkar <56784741+ShaswatJBabhulgaonkar@users.noreply.github.com>
2022-05-30 14:04:13 +0530

    Update tf_backend.tf

--- infra/iam/tf_backend.tf
+++ infra/iam/tf_backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "h1-data-science-tf-state"
+    bucket         = "data-science-tf-state"
     key            = "data-science/global/iam/tf.state"
     region         = "us-east-2"
-    dynamodb_table = "h1datascience-iam-tf-infra"
+    dynamodb_table = "datascience-iam-tf-infra"
   }
 }
